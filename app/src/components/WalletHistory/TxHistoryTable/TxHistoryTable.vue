09cd7645c53743e957d278387847fc86389ca6ad
Lionell Briones <llenoil@gmail.com>
2019-08-21 15:36:06 +0800

    #387 Added borders for the pagination

--- app/src/components/WalletHistory/TxHistoryTable/TxHistoryTable.vue
+++ app/src/components/WalletHistory/TxHistoryTable/TxHistoryTable.vue
@@ -90,7 +90,7 @@ export default {
     return {
       page: 1,
       pageCount: 0,
-      itemsPerPage: 8,
+      itemsPerPage: 2,
       expanded: [],
       pagination: {},
       defaultSort: 'date',
